615d0e5d00d8ca67edf007e6d05e6ae4039e56bd
Ajay kumar <31215701+ajay4q@users.noreply.github.com>
2021-09-01 14:26:54 +0530

    The Script without comments
    you can just download this if you know what to do.

--- script clean.bat
+++ script clean.bat
@@ -1,22 +1,43 @@
 cls
+
 @echo off
-title Pulse 
+
+title Pulse
+
 color 0a
+
+
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
+
+
+
 :LOCK
+
 ren youtube "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+
 echo Pulse locked
+
 goto End
+
+
+
 :UNLOCK
+
 set/p "pass=>"
+
 if NOT %pass%== letmeintenet747 goto End
+
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+
 ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" YouTube
+
 start explorer.exe "D:\video\YouTube"
+
 start D:\video\YouTube\youtubePlaylist.xspf
+
 goto End
+
+
 :End
-
-
-
